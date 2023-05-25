5bae22ac967974f012da8e81bf5eb0f2227600ac
devcom33 <mouad.elb06@gmail.com>
2023-05-25 14:49:14 -0700

    modification

---
+++ cmd.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * sig_handler - handler cntrl c
+ * @num: num argument
+ */
+void sig_handler(int num)
+{
+	(void)num;
+	write(STDOUT_FILENO, "\n$ ", _strlen("\n$ "));
+}
+/**
+ * prompt - a shell using c
+ * @arv: argument by user
+ * @envp: envirement variable argument
+ * @flg: flag argument for mode
+ */
+void prompt(char **arv, char **envp, bool flg)
+{
+	size_t n = 0;
+	ssize_t num_c = 0;
+	char *cmd = NULL, *rgv[MAX_C];
+	int x/*, stat,path*/;
+
+	while (1)
+	{
+		if (flg && isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, "$ ", _strlen("$ "));
+		signal(SIGINT, sig_handler);
+		num_c = getline(&cmd, &n, stdin);
+		if (num_c == -1) /*handles the end file case*/
+		{
+			free(cmd);
+			exit(EXIT_SUCCESS);
+		}
+		if (cmd[num_c - 1] == '\n')
+			cmd[num_c - 1] = '\0';
+		cmd = trim(cmd);
+		if (_strlen(cmd) == 0)
+			continue;
+		x = 0;
+		rgv[x] = strtok(cmd, " \n");
+		handle_exit(cmd);
+		handle_path(rgv, cmd);
+		while (rgv[x])
+		{
+			x++;
+			rgv[x] = strtok(NULL, " \n");
+		}
+		runcmd(rgv, arv, envp); /* envir */
+	}
+	free(cmd);
+}
--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,10 +7,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <stdbool.h>
-#define MAX_C 20
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <wait.h>
+#define MAX_C 10
 /**
  * struct denum - structure that contains vars
  * @cnt: lines cnt
@@ -33,28 +29,4 @@ char *_strncpy(char *dest, char *src, int n);
 /*void geterror(int cnt, char **arv, char *cmd);*/
 void geterror(denum *n, char **arv, char *cmd);
 /*void search_path(char **rgv, char *cmd, char **envp);*/
-char *get_path(char *cmd);
-char *_getenv(char *name);
-char **tokenize_env(char *path);
-extern char **environ;
-int _strncmp(char *str1, char *str2, int n);
-
-/**
-*
- *  * struct list_s - A new struct type defining a linked list.
- *   * @dir: A directory path.
- *    * @next: A pointer to another struct list_s.
- /
-typedef struct list_s
-{
-	char *dir;
-	struct list_s *next;
-} list_t;
-char **_getenv(const char *var);
-char *get_location(char *cmd);
-char *fill_path_dir(char *path);
-list_t *get_path_dir(char *path);
-list_t *add_node_end(list_t **head, char *dir);
-char *_strncat(char *dest, const char *src, size_t n);
-**/
 #endif

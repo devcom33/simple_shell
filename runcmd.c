b4b35cc538fd66c1759a4de2a54f68be3b73ae47
ussamaismaili <ismailiossama@gmail.com>
2023-05-26 15:11:52 -0700

    fix exit again 25

--- runcmd.c
+++ runcmd.c
@@ -28,7 +28,7 @@ void runcmd(char **rgv, char **arv, char **envp)
 			_strlen(": No such file or directory"));
 			write(STDOUT_FILENO, "\n", 1);
 			/*geterror(c -> cnt, arv, cmd);*/
-		}	
+		}
 		exit(EXIT_FAILURE);
 	}
 	else
